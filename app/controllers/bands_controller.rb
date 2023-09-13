f3d263dbdc1cda9975a467631f7f2d3ce2aae305
KeianRyu <k.ryu@saroute.co.jp>
2023-09-13 10:08:29 +0900

    add Member in band

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -5,7 +5,7 @@ class BandsController < ApplicationController
   def index
     @bands = Band.all
 
-    render json: @bands, only: [:name]
+    render json: @bands.includes(:members)
   end
 
   # GET /bands/1
